3cca52b1085151e2d88ca53c4c3ce2d5f598ec8a
Steve Idowu <idowusteve@ymail.com>
2020-05-05 15:13:41 +0100

    Nuxt client app

---
+++ nuxt-client/components/global/AppNav.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex justify-between mb-10">
+        <ul class="flex item-center">
+            <li class="mr-4">
+                <nuxt-link :to="{name: 'index'}" class="text-gray-800 font-medium">
+                Home
+            </nuxt-link>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li class="mr-4">
+                <nuxt-link :to="{name: 'account'}" class="text-gray-800 font-medium">
+                Name
+            </nuxt-link>
+            </li>
+            <li class="mr-4">
+                <a href="#" class="text-gray-800 font-medium">
+                Singout
+            </a>
+            </li>
+            <li class="mr-4">
+                <nuxt-link :to="{name: 'auth-signin'}" class="text-gray-800 font-medium">
+                Sign in
+            </nuxt-link>
+            </li>
+            <li class="mr-4">
+                <a href="#" class="text-gray-800 font-medium">
+                Sign up
+            </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ nuxt-client/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppNav />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/global/AppNav.vue'
+export default {
+  components:{
+    AppNav
+  }
+  
+}
+</script>
